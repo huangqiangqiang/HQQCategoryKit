4287677090468294d4b9fb0a5655621bb3d7434a
黄强强 <huangqiangqiang@lianluo.com>
2017-08-12 13:31:10 +0800

    修复手写键盘崩溃的bug

--- HQQCategoryKit/UIScrollView+HQQCategoryKit.m
+++ HQQCategoryKit/UIScrollView+HQQCategoryKit.m
@@ -6,8 +6,56 @@
 //
 //
 
-#import "UIScrollView+HQQCategoryKit.h"
+#import <Foundation/Foundation.h>
 
 @implementation UIScrollView (HQQCategoryKit)
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (![self isMemberOfClass:[UIScrollView class]]) {
+        
+    } else {
+        [[self nextResponder] touchesBegan:touches withEvent:event];
+        if ([super respondsToSelector:@selector(touchesBegan:withEvent:)]) {
+            [super touchesBegan:touches withEvent:event];
+        }
+    }
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (![self isMemberOfClass:[UIScrollView class]]) {
+        
+    } else {
+        [[self nextResponder] touchesMoved:touches withEvent:event];
+        if ([super respondsToSelector:@selector(touchesMoved:withEvent:)]) {
+            [super touchesMoved:touches withEvent:event];
+        }
+    }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (![self isMemberOfClass:[UIScrollView class]]) {
+        
+    } else {
+        [[self nextResponder] touchesEnded:touches withEvent:event];
+        if ([super respondsToSelector:@selector(touchesBegan:withEvent:)]) {
+            [super touchesEnded:touches withEvent:event];
+        }
+    }
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    if (![self isMemberOfClass:[UIScrollView class]]) {
+        
+    } else {
+        [[self nextResponder] touchesCancelled:touches withEvent:event];
+        if ([super respondsToSelector:@selector(touchesCancelled:withEvent:)]) {
+            [super touchesCancelled:touches withEvent:event];
+        }
+    }
+}
 
 @end
--- HQQCategoryKit/UITableView+HQQCategoryKit.m
+++ HQQCategoryKit/UITableView+HQQCategoryKit.m
@@ -6,8 +6,28 @@
 //
 //
 
-#import "UITableView+HQQCategoryKit.h"
+#import <Foundation/Foundation.h>
 
 @implementation UITableView (HQQCategoryKit)
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [[self nextResponder] touchesBegan:touches withEvent:event];
+}
+
+-(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [[self nextResponder] touchesMoved:touches withEvent:event];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [[self nextResponder] touchesEnded:touches withEvent:event];
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [[self nextResponder] touchesEnded:touches withEvent:event];
+}
 
 @end
