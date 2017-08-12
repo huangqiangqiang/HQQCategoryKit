f892b52ec4bbbf6cbd1c246a9b23bb10c264ca5b
黄强强 <huangqiangqiang@lianluo.com>
2017-08-12 13:11:59 +0800

    添加uitouch分类，实现事件传递

---
+++ HQQCategoryKit/UIScrollView+HQQCategoryKit.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+HQQCategoryKit.m
+//  Pods
+//
+//  Created by 黄强强 on 2017/8/12.
+//
+//
+
+#import "UIScrollView+HQQCategoryKit.h"
+
+@implementation UIScrollView (HQQCategoryKit)
+
+@end
---
+++ HQQCategoryKit/UITableView+HQQCategoryKit.m
@@ -0,0 +1,13 @@
+//
+//  UITableView+HQQCategoryKit.m
+//  Pods
+//
+//  Created by 黄强强 on 2017/8/12.
+//
+//
+
+#import "UITableView+HQQCategoryKit.h"
+
+@implementation UITableView (HQQCategoryKit)
+
+@end
